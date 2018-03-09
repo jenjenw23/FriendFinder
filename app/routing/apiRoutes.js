1a792bc31b9e4d1a084fb48c2caadfcbfb962c52
Woulas <jwoula01@presencehealth.net>
2018-03-09 16:29:53 -0600

    basic ajax to get friend array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,22 +2,22 @@ var friendsArray = require("../data/friend.js");
 
 module.exports = function(app) {
  
-  app.get("/api/home", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friendsArray);
   });
 
 
-  app.post("/api/tables", function(req, res) {
+  // app.post("/api/tables", function(req, res) {
     
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+  //   if (tableData.length < 5) {
+  //     tableData.push(req.body);
+  //     res.json(true);
+  //   }
+  //   else {
+  //     waitListData.push(req.body);
+  //     res.json(false);
+  //   }
+  // });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
